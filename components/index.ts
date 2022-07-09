39ed4aec7c24f75c524ad3ca80d3748ac6580d36
CaioDS <caioduartesantos2007@gmail.com>
2022-07-09 12:27:52 -0300

    Creation of Home page; Adding NFT metadata;

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+export { default as Navbar } from "./Navbar"
+
+export { default as BaseLayout } from './Layout/BaseLayout'
+
+export { default as NftList } from './Nft/List'
+
+export { default as NftItem } from './Nft/Item'
